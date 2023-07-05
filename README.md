a701581ab7fcbd31edf993adbbb6c05060e987ea
DevIncomin <bdr123654@gmail.com>
2023-07-05 08:00:42 +0300

    uploaded files to the verse
    initial D committing

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Misk-Shadowing-Program-Opportunities-Checker
- Long name short code, it basically checks their site for available opportunities, as I like to waste time writing useless code rather than checking manually.
+# Misk's JSP Opportunities Availability Checker
+
+For short it just sends GET requests to the site to get all information about the job shadowing, and then I just check whatever appeals to me so the code can check their availability instead, as automation is better, and also spending time coding is better than doing manual labor, I'm doing good for people who's after automation, I suppose.
---
+++ main.py
@@ -0,0 +1,94 @@
+# internal modules
+import requests
+import json # to process the GET request
+import datetime, time # logging, cooldown
+import webbrowser # to open web triggers
+
+
+# The amount of of number signs indicates comment's unimportance
+
+# change this to your account's:
+authBearer = "Bearer XXX"
+cooldown = 3 # never below 1, as it limits for 60 requests per minute for some things
+stopKey = "q" # Stops the script
+
+# logs
+logAvailables = True # after holding the stop key, it'll print the log of when an available chance is found
+allowWebTrigger = False # Allows to open the page below after it gets triggered, and only triggers once in a script's lifetime
+openSiteTrigger = "https://web.whatsapp.com/send/?phone=XXXXXXXXXXXX&text=X" # read above. can be "api" for application or "web" for WA web
+ballontipTrigger = True # Whenever it finds a chance, it'll do a notification.
+
+
+# external modules
+import keyboard # to check if key pressed or no, stopping the requests' loop
+if ballontipTrigger:
+    import ballontip # Windows' notifications, get "pypiwin32" if "win32api" isn't found
+
+
+## colors for aesthetics
+class colors:
+    end = "\033[0m"
+    black = "\033[90m"
+    red = "\033[91m"
+    green = "\033[92m"
+    yellow = "\033[93m"
+    blue = "\033[94m"
+    magenta = "\033[95m"
+    cyan = "\033[96m"
+    white = "\033[97m"
+
+# ids in the site:
+shadowingIds = [
+    "administration",
+    "accounting",
+    "marketing",
+    "law",
+    "it",
+    "film",
+    "medical",
+    "pharmacy",
+    "engineering"
+]
+
+# will change to your input's allowance
+targetIds = []
+
+i = 0
+for sId in shadowingIds:
+    print(f"{colors.blue}( {colors.green}{i}{colors.blue} ){colors.black} - {colors.white}{sId}{colors.end}")
+    i += 1
+
+for targetId in input(f"Input your targets' indexs: {colors.black}(0, 1, 2) {colors.yellow}").split(" "):
+    targetIds.append(shadowingIds[int(targetId)])
+print(f"{colors.end}")
+
+lastAvailables = []
+
+while not keyboard.is_pressed(stopKey):
+    request = requests.get(
+        "https://api.jsp.elham.training/api/students/opportunities/fields/riyadh",
+        headers={
+            "authorization" : authBearer
+        }
+    )
+
+    for x in json.loads(request.content):
+        if x["id"] in targetIds:
+            print(f"{colors.end}{x['id']} {f'{colors.green}available!{colors.end}' if x['count'] > 0 else f'{colors.red}unavailable{colors.end}'}") # forgive me,
+
+            if x['count'] > 0:
+                if logAvailables:
+                    lastAvailables.append(f"{x['id']}{colors.black} - {colors.end}{datetime.datetime.now()}")
+                
+                if allowWebTrigger:
+                    allowWebTrigger = False
+                    webbrowser.open(f'{openSiteTrigger}\n{x["id"]}')
+                    time.sleep(9) # just in case the site doesn't load fast, sites are bloated nowadays with nonsense
+                    keyboard.press_and_release("enter")
+                
+                if ballontipTrigger:
+                    ballontip.balloon_tip("Found a Chance!", x["name"]["en"].capitalize())
+    
+    time.sleep(cooldown)
+
+print(f"last availables: {lastAvailables if logAvailables else f'{colors.red}log disabled{colors.end}'}")
